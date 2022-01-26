c2d4b6b0c18563eb376744b49a13c624ef483ebf
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-26 18:28:37 -0300

    Temas

---
+++ src/Components/UI/temas.js
@@ -0,0 +1,20 @@
+import {
+  fundoClaro,
+  conteudoClaro,
+  textoFundoClaro,
+  fundoEscuro,
+  conteudoEscuro,
+  textoFundoEscuro,
+} from './variaveis';
+
+export const TemaClaro = {
+  body: fundoClaro,
+  inside: conteudoClaro,
+  text: textoFundoClaro
+}
+
+export const TemaEscuro = {
+  body: fundoEscuro,
+  inside: conteudoEscuro,
+  text: textoFundoEscuro
+}
