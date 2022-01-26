e7d57cf34171b500f41d42eb042b0669ee0d3e8f
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-26 14:29:38 -0300

    Props

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -18,17 +18,29 @@ const Logo = styled.img`
   width: 50px;
 `;
 
+const ButtonCabecalho = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  background: ${(props) => (props.primary ? 'white' : corPrimaria)};
+  color: ${(props) => (props.primary ? corPrimaria : 'white' )};
+`
+
 const Cabecalho = () => {
   return (
     <StyleHeader>
       <Logo className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        <ButtonCabecalho primary href="https://google.com">
           Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        </ButtonCabecalho>
+        <ButtonCabecalho href="https://google.com">
           Sair
-        </a>
+        </ButtonCabecalho>
       </div>
     </StyleHeader>
   );
