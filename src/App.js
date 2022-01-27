8d32ac1a9cc227cd24c6f64ffa8cc527d0fdacbb
LeonardoBrizolla <leonardobrizollars@gmail.com>
2022-01-26 22:07:07 -0300

    Troca de tema e setTema

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { ThemeProvider } from "styled-components";
 import { TemaEscuro, TemaClaro } from "./Components/UI/temas";
-
+import SwitcherTema from './Components/SwitcherTema';
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
+import { ButtonTema } from "./Components/UI";
 
 function App() {
+  const [tema, setTema] = useState(true);
+
   return (
-    <ThemeProvider theme={TemaEscuro}>
+    <ThemeProvider theme={tema ? TemaClaro : TemaEscuro}>
       <GlobalStyle />
+      <ButtonTema onClick={() => setTema(oldTema => !!!oldTema)}>
+        <SwitcherTema tema={tema}/>
+      </ButtonTema>
       <Cabecalho />
       <Container />
     </ThemeProvider>
